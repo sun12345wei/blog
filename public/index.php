1488cf4b5f320d219c1e51a2b115f6d58eae37dd
sunwei <sunwei@czxy.com>
2018-09-29 20:49:20 +0800

    '下载redis'

--- public/index.php
+++ public/index.php
@@ -1,6 +1,16 @@
 <?php
+// 使用 redis 保存 SESSION
+ini_set('session.save_handler', 'redis');
+// 设置 redis 服务器的地址、端
+ini_set('session.save_path', 'tcp://127.0.0.1:6379?database=3');
+
+session_start();
+
 // 定义常量
 define('ROOT', dirname(__FILE__) . '/../');
+
+// 引入 composer 自动加载文件
+require(ROOT.'vendor/autoload.php');
 
 // 实现类的自动加载
 function autoLoad($class)
@@ -12,25 +22,34 @@ function autoLoad($class)
 
 spl_autoload_register('autoLoad');
 
-// 添加路由 ：解析 URL 上的路径：   控制器/方法
-// 获取 URL 上的路径
+// 添加路由 ：解析 URL 浏览器上 blog/index CLI中就是 blog index
 
-if( isset($_SERVER['PATH_INFO']) )
+if(php_sapi_name() == 'cli')
 {
-    $pathInfo = $_SERVER['PATH_INFO'];
-    // 根据 / 转成数组
-    $pathInfo = explode('/', $pathInfo);
-
-    // 得到控制器名和方法名
-    $controller = ucfirst($pathInfo[1]) . 'Controller';
-    $action = $pathInfo[2];
+    $controller = ucfirst($argv[1]) . 'Controller';
+    $action = $argv[2];
 }
 else
 {
-    // 默认控制器和方法
-    $controller = 'IndexController';
-    $action = 'index';
+    if( isset($_SERVER['PATH_INFO']) )
+    {
+        $pathInfo = $_SERVER['PATH_INFO'];
+        // 根据 / 转成数组
+        $pathInfo = explode('/', $pathInfo);
+
+        // 得到控制器名和方法名
+        $controller = ucfirst($pathInfo[1]) . 'Controller';
+        $action = $pathInfo[2];
+    }
+    else
+    {
+        // 默认控制器和方法
+        $controller = 'IndexController';
+        $action = 'index';
+    }
 }
+
+
 
 
 // 为控制器添加命名空间
